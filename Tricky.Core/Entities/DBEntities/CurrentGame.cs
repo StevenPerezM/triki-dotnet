45ac3ad46c393696c54ee7aeb9ecb23c5c91f8e5
StevenPerezM <edwtiven@gmail.com>
2021-07-12 14:03:15 -0500

    feat: entities are added

---
+++ Tricky.Core/Entities/DBEntities/CurrentGame.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace Tricky.Core.Entities.DBEntities
+{
+    public class CurrentGame
+    {
+        public string id                                        { get; set; }
+        public DateTime startDate                               { get; set; }
+        public string turn                                      { get; set; }
+        public string status                                    { get; set; }
+        public DataPlayer player1                               { get; set; }
+        public DataPlayer player2                               { get; set; }
+        public string[,] dataCurrentMovements                   { get; set; }
+        public string winner                                    { get; set; }
+        public CurrentGame()
+        {
+            dataCurrentMovements = new string[3, 3];
+        }
+    }
+}
---
+++ Tricky.Core/Entities/DBEntities/DataPlayer.cs
@@ -0,0 +1,18 @@
+﻿using System.Collections.Generic;
+
+namespace Tricky.Core.Entities.DBEntities
+{
+    public class DataPlayer
+    {
+        public string id                { get; set; }
+        public string namePlayer        { get; set; }
+        public bool firstMovement       { get; set; }
+        public string indicator         { get; set; }
+        public List<string> movements   { get; set; }
+
+        public DataPlayer()
+        {
+            movements = new List<string>();
+        }
+    }
+}
