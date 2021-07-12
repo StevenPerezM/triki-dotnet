ed080a6f6443cf7594af3aef9fa1630ddde516c6
StevenPerezM <edwtiven@gmail.com>
2021-07-12 14:15:59 -0500

    feat: se crea servicio de excepciones personalizado

---
+++ Tricky.Core/Entities/Exceptions/GameTrickyException.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Runtime.Serialization;
+
+namespace Tricky.Core.Entities.Exceptions
+{
+    [Serializable]
+    public class GameTrickyException : Exception
+    {
+        public GameTrickyException(string message) : base(message)
+        {
+        }
+        protected GameTrickyException(SerializationInfo info, StreamingContext context) : base(info, context)
+        {
+        }
+        public override void GetObjectData(SerializationInfo info, StreamingContext context)
+        {
+            base.GetObjectData(info, context);
+        }
+    }
+}
