2480881fabcb672ceddbde42bc407eb80f381dbd
StevenPerezM <edwtiven@gmail.com>
2021-07-12 14:22:21 -0500

    feat: lambda is created that manages the players

---
+++ Tricky.Players/Startup.cs
@@ -0,0 +1,33 @@
+﻿using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using System;
+
+namespace Tricky.Players
+{    
+    public sealed class Startup
+    {
+        public Startup()
+        {
+        }
+
+        public void ConfigureServices(IServiceCollection services)
+        {
+            services.AddMvc();
+            services.AddCors();
+        }
+        public void Configure(IApplicationBuilder app)
+        {
+            try
+            {
+                 app.UseCors(x => x
+                .AllowAnyOrigin()
+                .AllowAnyMethod()
+                .AllowAnyHeader());
+            }
+            catch (Exception exception)
+            {
+                throw exception;
+            }
+        }
+    }
+}
